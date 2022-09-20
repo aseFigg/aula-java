40a57af558bf3bf624c5e3fcdf0c56b560f0c958
asefrigg <111322170+aseFigg@users.noreply.github.com>
2022-09-19 21:55:38 -0400

    Add files via upload

---
+++ aula java/HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld{
+
+    public static void main(String[] args){
+
+        int nota1 = 50;
+        int nota2 = 30;
+        int nota3 = 90;
+        // double media = (double) ((nota1 + nota2 + nota3)/ 3);
+        
+        boolean maior = (nota1 > nota2);
+
+        // if (media >= 70) {
+        //     System.out.println("você está aprovado!");
+        // } else {
+        //     double notaExame = 100 - media;
+
+        //     System.out.println("Exame! Você precisa de " + notaExame);
+        // } 
+        
+        
+        System.out.println(maior);
+
+    }
+
+}
